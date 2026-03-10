d4e5646beb45dd59f945ba067f39f556dc578593
walid hattab <walid.hattab@outlook.com>
2026-03-10 15:35:10 +0100

    update project_app views and urls, update base template, delete demo.html, update sidebar and footer

--- project/project_app/urls.py
+++ project/project_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books', views.books, name='books'),
     #path('about', views.about, name='about')
     #path('product', views.product, name='product')
     
--- project/project_app/views.py
+++ project/project_app/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #return HttpResponse("Hello, world. You're at the pages index.")
-    test ={
-        'name':'hghg',
-        'age': 1234567890
-    }
-    return render(request, 'pages/update.html',  test)
+    return render(request, 'pages/index.html')
+
+def books(request):
+    return render (request, 'pages/books.html')
